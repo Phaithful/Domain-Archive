7cf798443c3a4eccb29b628d58a2a4fb15d65964
Faithful Madu <madufaithful1@gmail.com>
2025-06-05 11:20:48 +0100

    Form to add Domain

---
+++ admin/add-domain.php
@@ -0,0 +1,173 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Add Domain</title>
+
+    <!-- Linking Files Documents -->
+    <link rel="stylesheet" href="assets/css/add-domain.css">
+    <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Sharp:opsz,wght,FILL,GRAD@48,400,0,0" rel="stylesheet" />
+    <link rel="preconnect" href="https://fonts.googleapis.com"><link rel="preconnect" href="https://fonts.gstatic.com" crossorigin><link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&family=Roboto+Slab:wght@100..900&display=swap" rel="stylesheet">
+
+    <!-- Linking Documents Ending -->
+
+</head>
+
+
+<body>
+    
+    <div class="container">
+
+
+
+        <!----------------- Collapsable Side Bar ----------------------->
+        <aside>
+
+            <!-- Top section of the side bar [Logo & Company Name]-->
+            <div class="top">
+                <div class="logo">
+                    <img src="../images/logo.png" alt="logo">
+                    <h2>Access <span class="logo-span">Solution</span></h2>
+                </div>
+
+                <div class="close" id="closeBtn">
+                    <span class="material-symbols-outlined">close</span>
+                </div>
+            </div>
+            <!-- Top section of the side bar ending -->
+
+
+            <!-- Actual Side Bar Menu/Links -->
+            <div class="sidebar">
+                
+                <a href="#">
+                    <span class="material-symbols-sharp">dashboard</span>
+                    <h3>Dashboard</h3>
+                </a>
+
+                <a href="#" >
+                    <span class="material-symbols-sharp">dns</span>                    
+                    <h3>Domain</h3>
+                </a>
+
+                <a href="#" class="active">
+                    <span class="material-symbols-sharp">add</span>
+                    <h3>Add Domain</h3>
+                </a>
+
+                <a href="#">
+                    <span class="material-symbols-sharp">bring_your_own_ip</span>
+                    <h3>Registrars</h3>
+                </a>
+
+                <a href="#">
+                    <span class="material-symbols-sharp">notifications</span>
+                    <h3>Messages</h3>
+                    <span class="notification-count">18</span>
+                </a>
+
+                <a href="#">
+                    <span class="material-symbols-sharp">settings</span>
+                    <h3>Settings</h3>
+                </a>
+
+
+                <a href="#">
+                    <span class="material-symbols-sharp">logout</span>
+                    <h3>Logout</h3>
+                </a>
+                
+
+            </div>
+            <!------------------- Actual Side Bar Menu ending ------------------------->
+
+        </aside>
+        <!---------------------- Collapsable Side Bar Ending --------------------------->
+
+
+        <!---------- Main Section --------------------->
+        <main>
+
+            <!-- Top of right section -->
+            <div class="top-right">
+
+                <div class="top">
+
+                    <button id="menu-btn">
+                        <span class="material-symbols-sharp">segment</span>
+                    </button>
+
+                    <div class="theme-toggler">
+                        <span class="material-symbols-sharp active">light_mode</span>
+                        <span class="material-symbols-sharp">dark_mode</span>
+                    </div>
+
+                    <div class="profile">
+                        <div class="info">
+                            <p style="display: flex;">Hey<b>, Faithful</b></p>
+                            <small class="text-muted">Admin</small>
+                        </div>
+
+                        <div class="profile-photo">
+                            <img src="../images/profile/male.png" alt="">
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+
+
+
+            <div class="main-section">
+                <h1>Add Domain</h1>
+
+                <div class="main-form">
+
+                    <div>
+                        <label for="domain-name">Domain Name</label>
+                        <input type="text" name="domain-name" class="domain-name">
+                    </div>
+                    
+                    <div>
+                        <label for="date-purchased">Date Purchased</label>
+                        <input type="date" name="date-purchased" class="date-purchased">
+                    </div>
+                    
+                    <div>
+                        <label for="expiry-date">Expiry Date</label>
+                        <input type="date" name="expiry-date" class="expiry-date">
+                    </div>
+
+                    <div>
+                        <label for="registrar">Registrar</label>
+                        <input type="text" name="registrar" class="registrar">
+                    </div>
+
+                    <button class="submit" id="submit">Add Domain</button>
+                        
+
+                </div>
+                
+            </div>
+            
+
+            
+
+        </main>
+        <!---------- Main Section Ending --------------------->
+
+
+    
+
+
+    </div>
+
+</body>
+
+
+</html>
---
+++ admin/dashboard.php
@@ -0,0 +1,353 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard</title>
+
+    <!-- Document Links -->
+    <link rel="stylesheet" href="assets/css/dashboard.css">
+    <link href="https://fonts.googleapis.com/css2?family=Material+Symbols+Sharp:opsz,wght,FILL,GRAD@48,400,0,0" rel="stylesheet" />
+    <link rel="preconnect" href="https://fonts.googleapis.com"><link rel="preconnect" href="https://fonts.gstatic.com" crossorigin><link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&family=Roboto+Slab:wght@100..900&display=swap" rel="stylesheet">
+
+    <!-- Document Links End -->
+
+</head>
+
+
+<body>
+    
+    
+    <div class="container">
+
+
+
+        <!----------------- Collapsable Side Bar ----------------------->
+        <aside>
+
+            <!-- Top section of the side bar [Logo & Company Name]-->
+            <div class="top">
+                <div class="logo">
+                    <img src="../images/logo.png" alt="logo">
+                    <h2>Access <span class="logo-span">Solution</span></h2>
+                </div>
+
+                <div class="close" id="closeBtn">
+                    <span class="material-symbols-outlined">close</span>
+                </div>
+            </div>
+            <!-- Top section of the side bar ending -->
+
+
+            <!-- Actual Side Bar Menu/Links -->
+            <div class="sidebar">
+                
+                <a href="#" class="active">
+                    <span class="material-symbols-sharp">dashboard</span>
+                    <h3>Dashboard</h3>
+                </a>
+
+                <a href="#" >
+                    <span class="material-symbols-sharp">dns</span>                    
+                    <h3>Domain</h3>
+                </a>
+
+                <a href="add-domain.php">
+                    <span class="material-symbols-sharp">add</span>
+                    <h3>Add Domain</h3>
+                </a>
+
+                <a href="#">
+                    <span class="material-symbols-sharp">bring_your_own_ip</span>
+                    <h3>Registrars</h3>
+                </a>
+
+                <a href="#">
+                    <span class="material-symbols-sharp">notifications</span>
+                    <h3>Messages</h3>
+                    <span class="notification-count">18</span>
+                </a>
+
+                <a href="#">
+                    <span class="material-symbols-sharp">settings</span>
+                    <h3>Settings</h3>
+                </a>
+
+
+                <a href="#">
+                    <span class="material-symbols-sharp">logout</span>
+                    <h3>Logout</h3>
+                </a>
+                
+
+            </div>
+            <!------------------- Actual Side Bar Menu ending ------------------------->
+
+        </aside>
+        <!---------------------- Collapsable Side Bar Ending --------------------------->
+
+
+        <!---------- Main Section --------------------->
+        <main>
+
+            <h1>Dashboard</h1>
+
+            <!------------------ Insight End --------------------->
+            <div class="insights">
+
+                <!--------------- Active Card ----------------->
+                <div class="active">
+                    <span class="material-symbols-sharp">domain</span>
+                    
+                    <div class="middle">
+                        <div class="left">
+                            <h3>Active</h3>
+                            <h1>45</h1>
+                        </div>
+                        <div class="progress">
+                            <svg>
+                                <circle cx='38'cy='38' r='36'></circle>
+                            </svg>
+                            <div class="number">
+                                <p>56.25%</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <small class="text-muted">
+                        last 24 Hours
+                    </small>
+                </div>
+                <!--------------- Active Card End ----------------->
+
+
+                <!--------------- Expire Soon Card ----------------->
+                <div class="soon">
+                    <span class="material-symbols-sharp">domain_disabled</span>
+                    
+                    <div class="middle">
+                        <div class="left">
+                            <h3>Expire Soon</h3>
+                            <h1>10</h1>
+                        </div>
+                        <div class="progress">
+                            <svg>
+                                <circle cx='38'cy='38' r='36'></circle>
+                            </svg>
+                            <div class="number">
+                                <p>12.5%</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <small class="text-muted">
+                        last 24 Hours
+                    </small>
+                </div>
+                <!--------------- Expire Soon Card End ----------------->
+
+
+                <!--------------- Expired Card ----------------->
+                <div class="expired">
+                    <span class="material-symbols-sharp">warning</span>
+                    
+                    <div class="middle">
+                        <div class="left">
+                            <h3>Expired</h3>
+                            <h1>25</h1>
+                        </div>
+                        <div class="progress">
+                            <svg>
+                                <circle cx='38'cy='38' r='36'></circle>
+                            </svg>
+                            <div class="number">
+                                <p>31.25%</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <small class="text-muted">
+                        last 24 Hours
+                    </small>
+                </div>
+                <!--------------- Expired Card End ----------------->
+
+            </div>
+            <!------------------ Insight End --------------------->
+
+
+            <div class="recent-domains">
+                <h2>Recent Domains Added</h2>
+                <table>
+
+                    <thead>
+                        <tr>
+                            <th>Domain Name</th>
+                            <th>Status</th>
+                            <th>Expiry Date</th>
+                            <th>Auto Renew</th>
+                            <th>Registrar</th>
+                            <th></th>
+                        </tr>
+                    </thead>
+
+                    <tbody>
+                        <tr>
+                            <td>Accesssolution.org</td>
+                            <td class="warning">Warning</td>
+                            <td>21-06-2025</td>
+                            <td>Yes</td>
+                            <td>Hostinger</td>
+                            <td class="primary">Action</td>
+                        </tr>
+
+                        <tr>
+                            <td>Garnet.ng.com</td>
+                            <td class="success">Active</td>
+                            <td>11-04-2027</td>
+                            <td>No</td>
+                            <td>GitHub</td>
+                            <td class="primary">Action</td>
+                        </tr>
+
+                        <tr>
+                            <td>environmental.com</td>
+                            <td class="success">Active</td>
+                            <td>07-02-2026</td>
+                            <td>No</td>
+                            <td>Hostinger</td>
+                            <td class="primary">Action</td>
+                        </tr>
+
+                        <tr>
+                            <td>Accesssolution.org</td>
+                            <td class="danger">Expired</td>
+                            <td>03-06-2025</td>
+                            <td>No</td>
+                            <td>Google Host</td>
+                            <td class="primary">Action</td>
+                        </tr>
+
+                        <tr>
+                            <td>spacg.ng</td>
+                            <td class="warning">Warning</td>
+                            <td>18-06-2025</td>
+                            <td>yes</td>
+                            <td>Hostinger</td>
+                            <td class="primary">Action</td>
+                        </tr>
+
+                        <tr>
+                            <td>gouni.edu.ng</td>
+                            <td class="success">Active</td>
+                            <td>30-07-2026</td>
+                            <td>No</td>
+                            <td>Hostinger</td>
+                            <td class="primary">Action</td>
+                        </tr>
+
+                    </tbody>
+
+                </table>
+                <a href="#">Show All</a>
+            </div>
+
+
+        </main>
+        <!---------- Main Section Ending --------------------->
+
+
+        <div class="right">
+
+            <!-- Top of right section -->
+            <div class="top">
+
+                <button id="menu-btn">
+                    <span class="material-symbols-sharp">segment</span>
+                </button>
+
+                <div class="theme-toggler">
+                    <span class="material-symbols-sharp active">light_mode</span>
+                    <span class="material-symbols-sharp">dark_mode</span>
+                </div>
+
+                <div class="profile">
+                    <div class="info">
+                        <p style="display: flex;">Hey<b>, Faithful</b></p>
+                        <small class="text-muted">Admin</small>
+                    </div>
+
+                    <div class="profile-photo">
+                        <img src="../images/profile/male.png" alt="">
+                    </div>
+
+                </div>
+
+            </div>
+            <!----------- Top of right section Ending ---------------->
+
+
+            <!-------------- Domain Analytics --------------->
+            <div class="domain-analytics">
+                <h2>Domain Analytics</h2>
+
+                <div class="item active">
+                    <span class="material-symbols-sharp">domain</span>
+                </div>
+            </div>
+
+
+            <!---------- Recent Updates ---------->
+            <div class="recent-updates">
+                <h2>Recent Updates</h2>
+                <div class="updates">
+
+                    <div class="update" >
+                        <div class="profile-photo">
+                            <span class="material-symbols-sharp">person</span>
+                        </div>
+                        <div class="message">
+                            <p><b>Edited Your Profile </b>You Changed your
+                            Profile Photo</p>
+                            <small class="text-muted">3 Hours Ago</small>
+                        </div>
+                    </div>
+
+                    <div class="update" >
+                        <div class="profile-photo">
+                            <span class="material-symbols-sharp">password</span>
+                        </div>
+                        <div class="message">
+                            <p><b>Changed Password </b>You Changed your
+                            Login Password</p>
+                            <small class="text-muted">17 Minutes Ago</small>
+                        </div>
+                    </div>
+
+                    <div class="update" >
+                        <div class="profile-photo">
+                            <span class="material-symbols-sharp">delete</span>
+                        </div>
+                        <div class="message">
+                            <p><b>Deleted Domain </b>You Deleted the 
+                            Gouni.ed.ng Domain</p>
+                            <small class="text-muted">1 Day Ago</small>
+                        </div>
+                    </div>
+                    
+                </div>
+            </div>
+
+            <!---------- End of Recent Updates ----------->
+
+        </div>
+
+
+    </div>
+    
+</body>
+
+
+</html>
